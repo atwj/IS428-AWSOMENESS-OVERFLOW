df11655d7b57eabdb56c96f36735dee1a1ef9053
Amos Tan <twjAmos@gmail.com>
2017-11-17 20:52:00 +0800

    Updated express app, vis2 code

---
+++ webapp/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express();
+const path = require('path')
+const router = express.Router()
+
+//static paths
+app.use(express.static('./public'))
+app.use('/', router)
+
+// Vis paths
+router.get('/', (req, res) => res.send('This is home'))
+router.get('/vis1', (req, res) => res.sendFile('public/www/index.html',{root:__dirname}))
+router.get('/vis2', (req, res) => res.sendFile('public/www/vis2.html',{root:__dirname}))
+router.get('/vis3', (req, res) => res.send('./hello world!'))
+
+app.listen(3000, () => console.log('App listening at localhost:3000'))
