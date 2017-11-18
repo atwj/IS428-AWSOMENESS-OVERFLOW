9d7c380486c29291f368edbe1b47a74bc0be0fa4
Ong Sue Cern <suecern.ong.2014@business.smu.edu.sg>
2017-11-18 15:15:02 +0800

    add btwnness

--- webapp/index.js
+++ webapp/index.js
@@ -12,5 +12,6 @@ router.get('/', (req, res) => res.send('This is home'))
 router.get('/vis1', (req, res) => res.sendFile('public/www/index.html',{root:__dirname}))
 router.get('/vis2', (req, res) => res.sendFile('public/www/vis2.html',{root:__dirname}))
 router.get('/vis3', (req, res) => res.sendFile('public/www/vis3.html',{root:__dirname}))
+router.get('/vis4', (req, res) => res.sendFile('public/www/btwnness.html',{root:__dirname}))
 
 app.listen(3000, () => console.log('App listening at localhost:3000'))
